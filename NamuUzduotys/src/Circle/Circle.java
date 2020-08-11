0b1431fcce8ba9b0595d0550dc4e8c810e0f6ed6
Modestas82 <siusk@inbox.lt>
2020-08-11 09:03:39 +0300

    Working

--- NamuUzduotys/src/Circle/Circle.java
+++
@@ -1,9 +0,0 @@
-package Circle;
-//Susikurti objektà Circle, kuris turës laukus (radius ir color) `Math.PI`- naudojama matematinei PI reikðmei gauti
-//(apskaièiuoti apskritimo ilgá)
-//(apskaièiuoti plotà) 
-//(iðspausdinti apskritimo spalvà)
-public class Circle {
-	
-
-}
